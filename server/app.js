796f687dcd617e811745cc1e0f3d0f9927bed44d
mokuku <omarbrahem@gmail.com>
2018-09-03 17:05:10 +0100

    more configs on the server side

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose=require('mongoose');
 
 const app = express();
 
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/graphqldb')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql:true
 }))
-app.listen(4000,()=>{
-    console.log('server listening on port 4000')
-})
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,56 @@
 const graphql = require('graphql');
 const _ = require('lodash')
-const { GraphQLObjectType , GraphQLString , GraphQLSchema,GraphQLID} = graphql;
+const { GraphQLObjectType ,
+     GraphQLString ,
+      GraphQLSchema,
+      GraphQLID,
+      GraphQLInt,
+      GraphQLList
+    } = graphql;
 
 //test data
 
 var books=[
-    {name:'book1', genre:'fifa', id:'1'},
-    {name:'book2', genre:'pes', id:'2'},
-    {name:'book3', genre:'fifa', id:'3'},
+    {name:'book1', genre:'fifa', id:'1',authorid:'1'},
+    {name:'book2', genre:'pes', id:'2',authorid:'2'},
+    {name:'book3', genre:'fifa', id:'3',authorid:'3'},
+    {name:'book4', genre:'fifa', id:'4',authorid:'2'},
+    {name:'book5', genre:'pes', id:'5',authorid:'3'},
+    {name:'book6', genre:'fifa', id:'6',authorid:'3'},
 ]
+var authors=[
+    {name:'charlie adams', age: 22, id:'1'},
+    {name:'john cenna', age: 34, id:'2'},
+    {name:'kylie lewis', age: 65, id:'3'},
+]
+
+
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields: ()=>({
         id: {type:GraphQLID},
         name: {type:GraphQLString},
-        genre: {type:GraphQLString}
+        genre: {type:GraphQLString},
+        author:{
+            type:Authortype,
+            resolve(parent,args){
+                return _.find(authors,{id:parent.authorid})
+            }
+        }
+    })
+})
+const Authortype = new GraphQLObjectType({
+    name:'Author',
+    fields: ()=>({
+        id: {type:GraphQLID},
+        name: {type:GraphQLString},
+        age: {type:GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return _.filter(books,{authorid:parent.id})
+            }
+        }
     })
 })
 
@@ -27,6 +63,27 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args){
                 return _.find(books,{id:args.id})
             }
+        },
+        author:{
+            type:Authortype,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return _.find(authors,{id:args.id})
+            }
+        },
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return books
+            }
+
+        },
+        authors:{
+            type:new GraphQLList(Authortype),
+            resolve(parent,args){
+                return authors
+            }
+
         }
     }
 });
